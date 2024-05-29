84168d547304932d1184a1d5170a8052d8e7549e
jelalooo <167035007+jelalooo@users.noreply.github.com>
2024-05-29 13:46:50 +0200

    Update komendy.sh

--- komendy.sh
+++ komendy.sh
@@ -22,12 +22,12 @@ git reset HEAD~1
 git checkout pushed
 git revert HEAD
 git cherry-pick C3 C4 C7
-git rebase -i overHere --solution-ordering C3,C5,C4
-git rebase -i main --solution-ordering C4
+git rebase -i overHere 
+git rebase -i main 
 git rebase bugFix main
-git rebase -i HEAD~2 --solution-ordering C3,C2
+git rebase -i HEAD~2 
 git commit --amend
-git rebase -i HEAD~2 --solution-ordering C2'',C3'
+git rebase -i HEAD~2 
 git rebase caption main
 git checkout main
 git cherry-pick C2
